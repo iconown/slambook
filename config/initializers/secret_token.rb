57620287e7769140be6708b57dd6ea6c2888cf0a
abdul k shaik <imran.ngage143@gmail.com>
2013-09-12 20:51:31 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slambook::Application.config.secret_token = '53ed12a5864ad38ca749bc693990673f8c313b02f25fdefa8521ba4895c8a8828de5f3b65a3893153ca80d9162ca6db4f48d3fb076c1ef48e9ffd1a755bce2cf'
